e80bb3d4d8f603ba80e92010574d65aa279f67d5
Aditya B <110244326+AdityaB2007@users.noreply.github.com>
2024-07-11 16:16:40 -0500

    Create UploadMoreRam.cpp

---
+++ UploadMoreRam.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int tc;
+  cin >> tc;
+  while (tc--){
+    int n, k;
+    cin >> n >> k;
+    cout << (n*k - k + 1) << "\n";
+  }
+  return 0;
+}
